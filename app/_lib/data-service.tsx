eee77f38cc9c932e9d266eb144f01d98acc3c2d2
elBanna00 <moaazibrahim3@gmail.com>
2024-06-13 06:08:50 +0300

    feat : add suspense to cabins page

--- app/_lib/data-service.tsx
+++ app/_lib/data-service.tsx
@@ -1,32 +1,30 @@
-// import { eachDayOfInterval } from 'date-fns';
-
+import { notFound } from "next/navigation";
+import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabaseClient";
 
-// /////////////
-// // GET
+/////////////
+// GET
 
-// export async function getCabin(id) {
-//   const { data, error } = await supabase
-//     .from('cabins')
-//     .select('*')
-//     .eq('id', id)
-//     .single();
+export async function getCabin(id: string) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("*")
+    .eq("id", id)
+    .single();
 
-//   // For testing
-//   // await new Promise((res) => setTimeout(res, 1000));
+  if (error) {
+    console.error(error);
+    notFound();
+  }
 
-//   if (error) {
-//     console.error(error);
-//   }
-
-//   return data;
-// }
+  return data;
+}
 
 // export async function getCabinPrice(id) {
 //   const { data, error } = await supabase
-//     .from('cabins')
-//     .select('regularPrice, discount')
-//     .eq('id', id)
+//     .from("cabins")
+//     .select("regularPrice, discount")
+//     .eq("id", id)
 //     .single();
 
 //   if (error) {
@@ -49,6 +47,9 @@ export const getCabins = async function () {
     .select("id, name, maxCapacity, regularPrice, discount, image")
     .order("name");
 
+  // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
+
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
@@ -60,9 +61,9 @@ export const getCabins = async function () {
 // // Guests are uniquely identified by their email address
 // export async function getGuest(email) {
 //   const { data, error } = await supabase
-//     .from('guests')
-//     .select('*')
-//     .eq('email', email)
+//     .from("guests")
+//     .select("*")
+//     .eq("email", email)
 //     .single();
 
 //   // No error here! We handle the possibility of no guest in the sign in callback
@@ -71,14 +72,14 @@ export const getCabins = async function () {
 
 // export async function getBooking(id) {
 //   const { data, error, count } = await supabase
-//     .from('bookings')
-//     .select('*')
-//     .eq('id', id)
+//     .from("bookings")
+//     .select("*")
+//     .eq("id", id)
 //     .single();
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Booking could not get loaded');
+//     throw new Error("Booking could not get loaded");
 //   }
 
 //   return data;
@@ -86,17 +87,17 @@ export const getCabins = async function () {
 
 // export async function getBookings(guestId) {
 //   const { data, error, count } = await supabase
-//     .from('bookings')
+//     .from("bookings")
 //     // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
 //     .select(
-//       'id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)'
+//       "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
 //     )
-//     .eq('guestId', guestId)
-//     .order('startDate');
+//     .eq("guestId", guestId)
+//     .order("startDate");
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Bookings could not get loaded');
+//     throw new Error("Bookings could not get loaded");
 //   }
 
 //   return data;
@@ -109,14 +110,14 @@ export const getCabins = async function () {
 
 //   // Getting all bookings
 //   const { data, error } = await supabase
-//     .from('bookings')
-//     .select('*')
-//     .eq('cabinId', cabinId)
+//     .from("bookings")
+//     .select("*")
+//     .eq("cabinId", cabinId)
 //     .or(`startDate.gte.${today},status.eq.checked-in`);
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Bookings could not get loaded');
+//     throw new Error("Bookings could not get loaded");
 //   }
 
 //   // Converting to actual dates to be displayed in the date picker
@@ -133,37 +134,37 @@ export const getCabins = async function () {
 // }
 
 // export async function getSettings() {
-//   const { data, error } = await supabase.from('settings').select('*').single();
+//   const { data, error } = await supabase.from("settings").select("*").single();
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Settings could not be loaded');
+//     throw new Error("Settings could not be loaded");
 //   }
 
 //   return data;
 // }
 
-export async function getCountries() {
-  try {
-    const res = await fetch(
-      "https://restcountries.com/v2/all?fields=name,flag"
-    );
-    const countries = await res.json();
-    return countries;
-  } catch {
-    throw new Error("Could not fetch countries");
-  }
-}
+// export async function getCountries() {
+//   try {
+//     const res = await fetch(
+//       "https://restcountries.com/v2/all?fields=name,flag"
+//     );
+//     const countries = await res.json();
+//     return countries;
+//   } catch {
+//     throw new Error("Could not fetch countries");
+//   }
+// }
 
 // /////////////
 // // CREATE
 
 // export async function createGuest(newGuest) {
-//   const { data, error } = await supabase.from('guests').insert([newGuest]);
+//   const { data, error } = await supabase.from("guests").insert([newGuest]);
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Guest could not be created');
+//     throw new Error("Guest could not be created");
 //   }
 
 //   return data;
@@ -171,7 +172,7 @@ export async function getCountries() {
 
 // export async function createBooking(newBooking) {
 //   const { data, error } = await supabase
-//     .from('bookings')
+//     .from("bookings")
 //     .insert([newBooking])
 //     // So that the newly created object gets returned!
 //     .select()
@@ -179,7 +180,7 @@ export async function getCountries() {
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Booking could not be created');
+//     throw new Error("Booking could not be created");
 //   }
 
 //   return data;
@@ -191,30 +192,30 @@ export async function getCountries() {
 // // The updatedFields is an object which should ONLY contain the updated data
 // export async function updateGuest(id, updatedFields) {
 //   const { data, error } = await supabase
-//     .from('guests')
+//     .from("guests")
 //     .update(updatedFields)
-//     .eq('id', id)
+//     .eq("id", id)
 //     .select()
 //     .single();
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Guest could not be updated');
+//     throw new Error("Guest could not be updated");
 //   }
 //   return data;
 // }
 
 // export async function updateBooking(id, updatedFields) {
 //   const { data, error } = await supabase
-//     .from('bookings')
+//     .from("bookings")
 //     .update(updatedFields)
-//     .eq('id', id)
+//     .eq("id", id)
 //     .select()
 //     .single();
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Booking could not be updated');
+//     throw new Error("Booking could not be updated");
 //   }
 //   return data;
 // }
@@ -223,11 +224,11 @@ export async function getCountries() {
 // // DELETE
 
 // export async function deleteBooking(id) {
-//   const { data, error } = await supabase.from('bookings').delete().eq('id', id);
+//   const { data, error } = await supabase.from("bookings").delete().eq("id", id);
 
 //   if (error) {
 //     console.error(error);
-//     throw new Error('Booking could not be deleted');
+//     throw new Error("Booking could not be deleted");
 //   }
 //   return data;
 // }
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,15 +1,14 @@
-import Counter from "@/app/_components/Counter";
-import CabinCard from "@/app/_components/CabinCard";
-import { CabinType, getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
+
+export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
-  // CHANGE
-  const cabins = await getCabins();
-
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,47 +23,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin: CabinType) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
-
-// import { Suspense } from "react";
-// // import CabinList from "../_components/CabinList";
-// // import Spinner from "../_components/Spinner";
-// // import Counter from "../_components/Counter";
-
-// export const revalidate = 3600;
-// // export const revalidate = 15;
-
-// export const metadata = {
-//   title: "Cabins",
-// };
-
-// export default function Page() {
-//   return (
-//     <div>
-//       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
-//         Our Luxury Cabins
-//       </h1>
-//       <p className="text-primary-200 text-lg mb-10">
-//         Cozy yet luxurious cabins, located right in the heart of the Italian
-//         Dolomites. Imagine waking up to beautiful mountain views, spending your
-//         days exploring the dark forests around, or just relaxing in your private
-//         hot tub under the stars. Enjoy nature&apos;s beauty in your own little
-//         home away from home. The perfect spot for a peaceful, calm vacation.
-//         Welcome to paradise.
-//       </p>
-
-//       <Suspense fallback={<Spinner />}>
-//         <CabinList />
-//       </Suspense>
-//     </div>
-//   );
-// }
