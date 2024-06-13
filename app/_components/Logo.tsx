902b16d6b132ee0542ba3428f02aafad61772aa0
elBanna00 <moaazibrahim3@gmail.com>
2024-06-13 05:31:47 +0300

    FREE PALASTINE!!

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -11,9 +11,16 @@ function Logo() {
         height="60"
         quality={100}
         width="60"
-        alt="The Wild Oasis logo"
+        alt="Scorch logo"
       />
+
       <span className="text-xl font-semibold text-primary-100">Scorch</span>
+      <span>
+        <span className=" text-red-600">Fr</span>
+        <span className=" text-black">ee </span>
+        <span className=" text-white">Pala</span>
+        <span className=" text-green-600">stine</span>
+      </span>
     </Link>
   );
 }
