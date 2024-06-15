1fabfd654b063a08e140fe35df56e0404be69708
elBanna00 <moaazibrahim3@gmail.com>
2024-06-16 02:41:19 +0300

    feat : adjust /accounst to be protected route

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,14 +1,19 @@
-import NextAuth, { NextAuthResult } from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 // import { createGuest, getGuest } from "./data-service";
 
-const authConfig = {
+const authConfig: NextAuthConfig = {
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
