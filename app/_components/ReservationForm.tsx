068682c56511755f988b8bb68e1758fa0147eb5c
elBanna00 <moaazibrahim3@gmail.com>
2024-06-16 02:28:31 +0300

    feat : Update Reservation form to be only for auth users

--- app/_components/ReservationForm.tsx
+++ app/_components/ReservationForm.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { User } from "next-auth";
 import { CabinType } from "../_lib/data-service";
 import { useReservation } from "./ReservationContext";
 
-function ReservationForm({ cabin }: { cabin: CabinType }) {
+function ReservationForm({
+  cabin,
+  user,
+}: {
+  cabin: CabinType;
+  user: User | undefined;
+}) {
   const { range } = useReservation();
   const { maxCapacity } = cabin;
 
@@ -12,21 +19,19 @@ function ReservationForm({ cabin }: { cabin: CabinType }) {
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
-          <img
-            // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
-          />
-          <p>{user.name}</p>
-        </div> */}
+        <div className="flex gap-4 items-center">
+          {user && (
+            <img
+              // Important to display google profile images
+              referrerPolicy="no-referrer"
+              className="h-8 rounded-full"
+              src={user.image!}
+              alt={user.name!}
+            />
+          )}
+          <p>{user?.name}</p>
+        </div>
       </div>
-
-      <p>
-        {String(range.from)} to {String(range.to)}
-      </p>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
         <div className="space-y-2">
